d8e69bc5ee2ac5b3bef34fcce2ce3429149aacee
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-24 22:20:00 +0600

    phpclass2

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $name = 'Md. Zia Uddin';
+    echo $name . '<br/>';
+
+    $age = '23 Years';
+    echo $age . '<br/>';
+
+    $address = 'West Mekhal, Hathazari, Chittagong.';
+    echo $address;
+?>
